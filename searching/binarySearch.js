48a063d3c7e95a46086f966144436d1a497d0989
Antarip Chatterjee <chatterjeeantarip1234@gmail.com>
2022-10-19 14:51:04 +0530

    co

--- searching/binarySearch.js
+++ searching/binarySearch.js
@@ -1,3 +1,18 @@
-function binarySearch(arr,n,key){
-    
+function binarySearch(arr,s,n,key){
+    let mid = Math.floor((n+s)/2);
+    if(key == arr[mid]){
+        return mid;
+    }
+    if(key > arr[mid]){
+        return binarySearch(arr,mid+1,n,key);
+    }
+    if(key < arr[mid]){
+        return binarySearch(arr,s,mid-1,key);
+    }
 }
+
+let arr = [1,2,3,4,5,6];
+
+let n  = arr.length;
+
+console.log(binarySearch(arr,0,n-1,4));
