4976ec017bf817b1c86b952dfc1e0d5c4cccc17d
Antarip Chatterjee <chatterjeeantarip1234@gmail.com>
2023-08-09 20:27:02 +0530

    commit

---
+++ searching/9Aug.js
@@ -0,0 +1,28 @@
+// Qustion 
+
+//     1 2 3
+//     4 5 6
+//     7 8 9
+
+//     Ans 1 4 7 8 5 2 3 5 9 
+
+
+// function arr(arr){
+
+
+
+// };
+
+let arr = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]];
+
+
+//console.log(arr.length);
+
+// for(let i = 0; i < arr.length;i++){
+//     console.log(arr[i]);
+// }
+
+console.log(arr[0][0],arr[1][0],arr[2][0]);
