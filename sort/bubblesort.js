6ddcc1c1ea742797aabcbb0d3481e8d38fca357e
Antarip Chatterjee <chatterjeeantarip1234@gmail.com>
2023-08-10 19:33:32 +0530

    Commit

--- sort/bubblesort.js
+++ sort/bubblesort.js
@@ -7,7 +7,7 @@ function swap(arr,a,b){
 }
 
 function bubblesort(arr,n){
-    for(let i = 0; i < n-1;i++){
+    for(let i = 0; i < n - 1;i++){
         for(let j = 0; j < n - i - 1; j++){
             if(arr[j] > arr[j+1] ){
                 swap(arr,j,j+1);
